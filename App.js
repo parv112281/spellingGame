fae89e7a1e710dc26e6c2445833224d5dde16e5a
Parv Singh <parv112281@gmail.com>
2018-03-28 20:06:41 -0400

    The words are now being enunciated by app and the spelled out, highlighting each letter as it is said out loud.

--- App.js
+++ App.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
+import {View, StyleSheet} from 'react-native';
 import { Constants } from 'expo';
-
-// You can import from local files
-import AssetExample from './components/AssetExample';
+import Word from './components/Word.js'
 
 // or any pure javascript modules available in npm
 import { Card } from 'react-native-elements'; // Version can be specified in package.json
@@ -11,25 +9,13 @@ import { Card } from 'react-native-elements'; // Version can be specified in pac
 export default class App extends Component {
   constructor() {
     super()
-    this.state = {
-      index: 0
-    }
   }
-
-  nextWord() {
-    this.setState(prevState => ({
-      index: ++prevState.index
-    }))
-    //console.log(this.state.index)
-  }
-
   render() {
     return (
       <View style={styles.container}>
         <Card title="Spelling Word">
-          <AssetExample index={this.state.index}/>
+          <Word />
         </Card>
-        <Button onPress={()=>this.nextWord()} title='Next' />
       </View>
     );
   }
@@ -42,12 +28,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     paddingTop: Constants.statusBarHeight,
     backgroundColor: '#ecf0f1',
-  },
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    color: '#34495e',
   },
 });
--- components/Word.js
+++ components/Word.js
@@ -1,42 +1,103 @@
 import React, {Component} from 'react'
-import {Text, StyleSheet} from 'react-native'
+import {View, Text, Button, StyleSheet} from 'react-native'
 import {Speech} from 'expo'
 
 const styles = StyleSheet.create({
-  word: {fontSize: 72}
+  word: {fontSize: 72},
+  highlight_letter: {fontSize: 96, color:"red"},
 })
 
 export default class Word extends Component {
 
-  constructor() {
-    super()
-    this.words = ['teeth', 'baby', 'silly', 'community']
-    this.wordListLength = this.words.length
-  }
+    words = ['one', 'two', 'three', 'four']
+    wordListLength = this.words.length
+    state = {
+      buttonDisabled: true,
+      index: 0,
+      currLetterIndex: 0,
+      letterHighlight: false
+    }
+    letterIndex = 0
+
 
   componentDidMount() {
-    this.soundAndSpellWord(this.words[0])
+    this.speakWord()
   }
 
-  async soundAndSpellWord(currWord) {
-    await Speech.speak(currWord)
-    //console.log(currWord)
-    for(let letter of currWord) {
-      await Speech.speak(letter)
-      //console.log(letter)
-    }
+  nextWord = async () => {
+    await this.setState(prevState => ({
+      index: ++prevState.index,
+      buttonDisabled: true,
+      currLetterIndex: 0,
+      letterHighlight: true,
+    }))
+    this.speakWord()
   }
 
-  componentDidUpdate() {
-    let currWord = this.words[this.props.index % this.wordListLength]
-    this.soundAndSpellWord(currWord)
+  speechStart = {
+    onStart: this.letterStarted,
+    onDone: this.letterEnded,
+  }
+
+  letterStarted = () => {
+    this.setState(prevState => ({
+      currLetterIndex: prevState.currLetterIndex,
+      index: prevState.index,
+      buttonDisabled: prevState.buttonDisabled,
+      letterHighlight: true,
+    }))
+  }
+
+ letterEnded = () => {
+    this.setState(prevState => ({
+      currLetterIndex: ++prevState.currLetterIndex,
+      index: prevState.index,
+      buttonDisabled: prevState.buttonDisabled,
+      letterHighlight: false,
+    }))
+  }
+
+   speakWord = async () => {
+      let currWord = this.words[this.state.index % this.wordListLength]
+
+      for(let letter of currWord) {
+        await Speech.speak(letter, {
+          onStart: this.letterStarted,
+          onDone: this.letterEnded,
+          })
+        }
+      await Speech.speak(currWord, {onDone: this.enableButton})
+  }
+
+  enableButton = () => {
+    this.setState(prevState => ({
+      buttonDisabled: !prevState.buttonDisabled,
+      index: prevState.index,
+      currLetterIndex: prevState.currLetterIndex,
+      letterHighlight: prevState.letterHighlight,
+    }))
   }
 
   render() {
-    let word = this.words[this.props.index % this.wordListLength]
-    //Speech.speak(word)
+    let word = this.words[this.state.index % this.wordListLength]
+    let word_part_beg = word.substring(0, this.state.currLetterIndex)
+    let highlight_letter = word.substring(this.state.currLetterIndex, this.state.currLetterIndex + 1)
+    let word_part_end = word.substring(this.state.currLetterIndex + 1)
+    if(this.state.letterHighlight)
+    {
+      return(
+        <View>
+          <Text style={styles.word}>{word_part_beg}<Text style={styles.highlight_letter}>{highlight_letter}</Text>{word_part_end}</Text>
+          <Button onPress={()=>this.nextWord()} title='Next' disabled={this.state.buttonDisabled} />
+        </View>
+      )
+    }
+
     return(
-      <Text style={styles.word}>{word}</Text>
+      <View>
+        <Text style={styles.word}>{word}</Text>
+        <Button onPress={()=>this.nextWord()} title='Next' disabled={this.state.buttonDisabled} />
+      </View>
       )
   }
 }
